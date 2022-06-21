36ea176397afd3c3d251c6ba27965c54555791ca
Allison H <eherazo@lsv-tech.com>
2022-06-21 11:07:07 -0500

    creacion splash

--- mi_primer_app/lib/main.dart
+++ mi_primer_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mi_primer_app/view/list_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:mi_primer_app/view/splash.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,10 +16,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Material App',
-        home: ListView1());
+        home: const Splash());
   }
 }
 
